16213e83fe96caa0de44901960511f757615ad70
Leo Nash <hello@leonash.net>
2026-05-28 01:28:46 +0000

    Add configuration knob to enable 0FC channels
    In upcoming commits we will read this knob to determine whether to
    negotiate 0FC channels.
    
    For now, we make a best-effort attempt to make sure the configured chain
    source supports 0FC channels if this knob is set.
    
    Do this roundtrip at the same time we make a roundtrip to retrieve the
    feerates to keep startup as fast as possible.

--- src/config.rs
+++ src/config.rs
@@ -60,7 +60,8 @@ pub const DEFAULT_LOG_FILENAME: &'static str = "ldk_node.log";
 /// The default storage directory.
 pub const DEFAULT_STORAGE_DIR_PATH: &str = "/tmp/ldk_node";
 
-// The default Esplora server we're using.
+// The default Esplora server we're using. It supports `submitpackage`, check using POST on the
+// `/txs/package` endpoint.
 pub(crate) const DEFAULT_ESPLORA_SERVER_URL: &str = "https://blockstream.info/api";
 
 /// The default stop gap used for BDK full scans of the on-chain wallet.
@@ -305,10 +306,11 @@ impl Default for HumanReadableNamesConfig {
 ///
 /// ### Defaults
 ///
-/// | Parameter                  | Value  |
-/// |----------------------------|--------|
-/// | `trusted_peers_no_reserve` | []     |
-/// | `per_channel_reserve_sats` | 25000  |
+/// | Parameter                     | Value  |
+/// |-------------------------------|--------|
+/// | `trusted_peers_no_reserve`    | []     |
+/// | `per_channel_reserve_sats`    | 25000  |
+/// | `enable_zero_fee_commitments` | false  |
 ///
 ///
 /// [BOLT 3]: https://github.com/lightning/bolts/blob/master/03-transactions.md#htlc-timeout-and-htlc-success-transactions
@@ -344,6 +346,14 @@ pub struct AnchorChannelsConfig {
 	/// might not suffice to successfully spend the Anchor output and have the HTLC transactions
 	/// confirmed on-chain, i.e., you may want to adjust this value accordingly.
 	pub per_channel_reserve_sats: u64,
+	/// In addition to `option_anchors_zero_fee_htlc_tx`, we will also attempt to negotiate
+	/// `option_zero_fee_commitments`. Zero-fee commitment channels remove all commitment
+	/// feerate negotiation from the channel, and instead source *all* the fees required to
+	/// confirm the commitment from the anchor reserve at the time of broadcast. If set, your
+	/// chain source *must* support the `submitpackage` Bitcoin Core RPC, and relay TRUC, P2A,
+	/// and ephemeral dust.
+	/// See [BOLT 3] for more technical details.
+	pub enable_zero_fee_commitments: bool,
 }
 
 impl Default for AnchorChannelsConfig {
@@ -351,6 +361,7 @@ impl Default for AnchorChannelsConfig {
 		Self {
 			trusted_peers_no_reserve: Vec::new(),
 			per_channel_reserve_sats: DEFAULT_ANCHOR_PER_CHANNEL_RESERVE_SATS,
+			enable_zero_fee_commitments: false,
 		}
 	}
 }
