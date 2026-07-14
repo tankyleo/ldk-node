1d507ae8b5efc88547117ae9c8813d7f1a48d68e
Leo Nash <hello@leonash.net>
2026-07-14 01:35:58 +0000

    f: move 0fc support required to own variable

--- src/lib.rs
+++ src/lib.rs
@@ -308,6 +308,8 @@ impl Node {
 					})
 					.unwrap_or(false)
 			});
+		let zero_fee_commitments_support_required = any_current_0fc_channels
+			|| self.config.anchor_channels_config.enable_zero_fee_commitments;
 
 		// Block to ensure we update our fee rate cache once on startup.
 		// Also take this opportunity to make sure our chain source supports 0FC channels
@@ -319,8 +321,7 @@ impl Node {
 			tokio::try_join!(
 				chain_source.update_fee_rate_estimates(),
 				chain_source.validate_zero_fee_commitments_support_if_required(
-					any_current_0fc_channels
-						|| self.config.anchor_channels_config.enable_zero_fee_commitments
+					zero_fee_commitments_support_required
 				)
 			)
 		})?;
